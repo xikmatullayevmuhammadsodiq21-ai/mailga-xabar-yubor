acd7edc6aa1e1e33dbb6281446813a1a2f23185c
Muhammadsodiq <xikmatullayevmuhammadsodiq21@gmail.com>
2026-03-26 12:57:48 +0500

    a

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY", default="django-insecure-temp-key")
 DEBUG = env.bool("DEBUG", default=True)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost", 'SendMailsite.pythonanywhere.com'])
 
 AUTH_USER_MODEL = 'users.User'
 
